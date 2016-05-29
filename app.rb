a3d2599f010cbdee92c284c2f16cdb9e701b5bbd
Robert Dzieża <dziezarobert@gmail.com>
2016-05-29 10:37:57 +0200

    creating pdf docs added

--- app.rb
+++ app.rb
@@ -1,6 +1,6 @@
 require 'sinatra'
 require 'haml'
-
+require 'prawn'
 
 get '/' do
   haml :home
@@ -16,13 +16,27 @@ post '/upload' do
 end
 
 get '/list' do
-	@files = Dir.entries("files").select {|f| !File.directory? f}
+	# @files = Dir.entries("files").select {|f| !File.directory? f}
+	@files = Dir.glob("files/*.{jpg,gif}")
 	haml :list
 end
 
 post '/save' do
 	result = ""
 	params[:files].each {|f| result += f.to_s}
+	
+	
+	pdf = Prawn::Document.new
+
+	params[:files].each do |f|
+		title = "files/" + f.to_s
+		pdf.image title, :at => [50, 250], :width => 300, :height => 350
+		pdf.start_new_page
+	end
+
+
+	pdf.render_file "files/outcome.pdf"
+
 	result
 end
 

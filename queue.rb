680a73bb3603ce125e6a98068374e782411d6676
tsovek <tomasz.a.sowa@gmail.com>
2016-06-05 19:50:48 +0200

    ext

---
+++ queue.rb
@@ -0,0 +1,63 @@
+require 'sinatra'
+require 'aws-sdk'
+require 'pony'
+require 'sendmail'
+require 'prawn'
+require 'json'
+
+def send_mail_from_queue
+  s3_client = Aws::S3::Client.new(region: 'eu-central-1')  
+  queue = sqs.create_queue({queue_name: "tsowa-queue_name"})  
+  
+  resp = s3_client.receive_message({
+    queue_url: queue.queue_url,
+    message_attribute_names: ["MessageAttributeName"],
+    max_number_of_messages: 1,
+    visibility_timeout: 1,
+    wait_time_seconds: 1,
+  })
+  
+  msg = JSON.parse(resp.body)
+  album_name = msg["album_name"]
+  email = msg["email"]
+  files = msg["files"]
+  puts album_name
+  puts email
+  puts files
+  if (album_name.nil? && email.nil? && files.nil?)
+  
+    FileUtils.mkdir_p 'files' # temporary directory
+    files.each do |filename|
+      # save every choosed files to files/ directory
+      s3_client.get_object(
+      bucket: '166543-robson', 
+      key: filename, 
+      response_target: "files/" + filename)
+    end
+
+    pdf = Prawn::Document.new
+    files.each do |f|
+      title = "files/" + f # path to file
+      pdf.image title, :at => [50, 250], :width => 300, :height => 350
+      pdf.start_new_page
+    end
+
+    pdf.render_file "files/" + name # save pdf to file
+    
+    # send mail
+    Pony.mail(
+      :to => email, 
+      :from => 'fake@wpc2016.uek.krakow.pl', 
+      :subject => "Your album: #{name}", 
+      :body => 'Check attachments.',
+      :attachments => {"#{name}" => File.read("files/" + name) })
+        
+    # delete files from bucket, remove temporary dir
+    FileUtils.remove_dir "files";
+    files.each do |f|
+      obj = get_bucket.object(f)
+      obj.delete
+    end
+  end
+end
+

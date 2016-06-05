bc61a585fd7d216317e59282d54d0166e1c894bb
tsovek <tomasz.a.sowa@gmail.com>
2016-06-05 20:14:55 +0200

    fix

--- queue.rb
+++ queue.rb
@@ -19,52 +19,50 @@ while true
   })
   
   if resp.nil?
-    puts resp.to_h
-  end
-  
-  msg = JSON.parse(resp.body)
-  album_name = msg["album_name"]
-  email = msg["email"]
-  files = msg["files"]
-  if (album_name.nil? && email.nil? && files.nil?)
-    FileUtils.mkdir_p 'files' # temporary directory
-    files.each do |filename|
-      # save every choosed files to files/ directory
-      s3_client.get_object(
-      bucket: '166543-robson', 
-      key: filename, 
-      response_target: "files/" + filename)
-    end
+    msg = JSON.parse(resp.body)
+    album_name = msg["album_name"]
+    email = msg["email"]
+    files = msg["files"]
+    if (album_name.nil? && email.nil? && files.nil?)
+      FileUtils.mkdir_p 'files' # temporary directory
+      files.each do |filename|
+        # save every choosed files to files/ directory
+        s3_client.get_object(
+        bucket: '166543-robson', 
+        key: filename, 
+        response_target: "files/" + filename)
+      end
 
-    pdf = Prawn::Document.new
-    files.each do |f|
-      title = "files/" + f # path to file
-      pdf.image title, :at => [50, 250], :width => 300, :height => 350
-      pdf.start_new_page
-    end
+      pdf = Prawn::Document.new
+      files.each do |f|
+        title = "files/" + f # path to file
+        pdf.image title, :at => [50, 250], :width => 300, :height => 350
+        pdf.start_new_page
+      end
 
-    pdf.render_file "files/" + name # save pdf to file
-    
-    # send mail
-    Pony.mail(
-      :to => email, 
-      :from => 'fake@wpc2016.uek.krakow.pl', 
-      :subject => "Your album: #{name}", 
-      :body => 'Check attachments.',
-      :attachments => {"#{name}" => File.read("files/" + name) })
-        
-    # delete files from bucket, remove temporary dir
-    FileUtils.remove_dir "files";
-    files.each do |f|
-      obj = get_bucket.object(f)
-      obj.delete
+      pdf.render_file "files/" + name # save pdf to file
+      
+      # send mail
+      Pony.mail(
+        :to => email, 
+        :from => 'fake@wpc2016.uek.krakow.pl', 
+        :subject => "Your album: #{name}", 
+        :body => 'Check attachments.',
+        :attachments => {"#{name}" => File.read("files/" + name) })
+          
+      # delete files from bucket, remove temporary dir
+      FileUtils.remove_dir "files";
+      files.each do |f|
+        obj = get_bucket.object(f)
+        obj.delete
+      end
+      
+      resp = client.delete_message({
+        queue_url: queue.queue_url, # required
+        receipt_handle: "String", # required
+      })
+      puts resp.to_h
     end
-    
-    resp = client.delete_message({
-      queue_url: queue.queue_url, # required
-      receipt_handle: "String", # required
-    })
-    puts resp.to_h
   end
 end
 

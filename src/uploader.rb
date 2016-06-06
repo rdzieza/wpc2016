c22570af5ef9045f90322c882294e06df7890406
Mateusz Zając <zajac.mateusz@icloud.com>
2016-06-06 07:49:12 +0200

    Small refactoring

---
+++ src/uploader.rb
@@ -0,0 +1,32 @@
+require 'aws-sdk'
+
+class Uploader
+
+  def self.upload_to_bucket(file, filename)
+    obj = get_bucket.object(filename.downcase)
+    obj.upload_file(file) ? puts "Uploaded #{file}" : puts "Could not upload #{file}!"
+  end
+
+  def self.send_to_sqs(json)
+    sqs = get_sqs
+    sqs.send_message({
+                         queue_url: sqs.queue_url,
+                         message_body: json,
+                         delay_seconds: 1
+                     })
+  end
+
+  private
+
+  AWS_REGION = 'eu-central-1'
+
+  def self.get_bucket
+    Aws::S3::Resource.new(region: AWS_REGION).bucket('166543-robson')
+  end
+
+  def self.get_sqs
+    client = Aws::SQS::Client.new(region: AWS_REGION)
+    client.create_queue({queue_name: 'tsowa-queue_name'})
+  end
+
+end
